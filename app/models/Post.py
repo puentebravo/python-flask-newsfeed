4ff2695f01c79fbcc73958c3027466f9a8e0fad1
Puentebravo <puentematos@gmail.com>
2021-06-11 21:27:21 -0400

    Added vote count column to Post table

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,7 +1,10 @@
 from datetime import datetime
+
+from sqlalchemy.sql.expression import select
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from .Vote import Vote
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+from sqlalchemy.orm import column_property, relationship
 
 class Post(Base):
     __tablename__ = "posts"
@@ -11,6 +14,9 @@ class Post(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now)
+    vote_count = column_property(
+        select([func.count(Vote.id)]).where(Vote.post_id == id)
+    )
     user = relationship("User")
     comments = relationship("Comment", cascade="all,delete")
 
