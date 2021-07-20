2894c4605c22bb80dbd9a977be1704ba45f2b16a
Puentebravo <puentematos@gmail.com>
2021-07-20 18:10:35 -0400

    re-wrote get_db function to store database connection within app context

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,7 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, engine
 from sqlalchemy.orm import create_session, sessionmaker
 from dotenv import load_dotenv
+from sqlalchemy.orm import session
 from sqlalchemy.orm.session import Session
+from flask import g
+
 
 load_dotenv()
 
@@ -11,3 +14,14 @@ load_dotenv()
 engine = create_engine(getenv("DB_URL"), echo=True, pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def init_db():
+    Base.metadata.create_all(engine)
+
+def get_db():
+    if "db" not in g: 
+        #this stores Database connetion within the app context
+        g.db = Session()
+    return g.db
+
+    
